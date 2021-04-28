bec7ef47972d001b89b346662a7ad43ed430c62b
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-28 15:17:01 +0200

    Added new UI for LocationViewController

---
+++ UberClone/Controller/LocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LocationViewController.swift
+//  UberClone
+//
+//  Created by Özgün Yildiz on 28.04.21.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController, UITableViewDataSource {
+    
+    var locations = [Location]()
+    var pickupLocation: Location?
+    var dropoffLocation: Location?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locations = LocationService.shared.returnLocations()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell") as! LocationCell
+        let location = locations[indexPath.row]
+        cell.update(location: location)
+        return cell
+    }
+    
+    
+}
