ea25a89252f40fd8f5b6c86ddd09becfb7ab3b5a
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-28 16:14:51 +0200

    Added code description

--- UberClone/Controller/LocationViewController.swift
+++ UberClone/Controller/LocationViewController.swift
@@ -6,20 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
-class LocationViewController: UIViewController, UITableViewDataSource {
+class LocationViewController: UIViewController, UITableViewDataSource, UITextFieldDelegate {
     
     var locations = [Location]()
     var pickupLocation: Location?
     var dropoffLocation: Location?
+    // we need this, because we want the text the user types into the dropoffTextField to autocomplete with the corresponding location
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
     @IBOutlet weak var dropoffTextfield: UITextField!
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         locations = LocationService.shared.returnLocations()
+        // when we open the VC, the textfield is being automatically selected
+        // (becomesFirstResponder) and the keyboard is being pushed up
         dropoffTextfield.becomeFirstResponder()
+        dropoffTextfield.delegate = self
+    }
+    
+    // delegate method; If the textfield was typed in (dropOffTextfield), we are notifying the LocationViewController (which is the delegate) that changes happened (with every single change in the textField (deletions, modifications, additions)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // we are replacing what was previously in the textField with the range that the user wants to update and the update that the user wants to make. The result of this is being stored in latestString
+        let latestString = (textField.text as! NSString).replacingCharacters(in: range, with: string)
+        print("latest String \(latestString)")
+        return true
     }
     
     
